e40ba47c004f01407fe196aaa573fa73ba4f9a59
Rory <rory_94@live.co.uk>
2021-04-03 15:42:39 +0100

    updated order of projects

--- main.js
+++ main.js
@@ -4,3 +4,13 @@ const navbarLinks = document.getElementsByClassName('navbar__links')[0]
 toggleButton.addEventListener('click', () => {
   navbarLinks.classList.toggle('active')
 })
+
+document.querySelector('.nav__links').addEventListener('click', function(e) {
+  e.preventDefault();
+
+  // Matching strategy
+  if (e.target.classList.contains('nav--link')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
+});
